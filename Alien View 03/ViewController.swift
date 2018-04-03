690aa0fea036c53d00f0d1a236c5993c8705ce85
DIT <d7703_21@d7703-21ui-imac.local>
2018-04-03 15:34:00 +0900

    circle and one derection

--- Alien View 03/ViewController.swift
+++ Alien View 03/ViewController.swift
@@ -9,17 +9,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var count = 1;
+    var d = true;
+    
+    @IBOutlet weak var lblC: UILabel!
+    @IBOutlet weak var imgView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        imgView.image = UIImage(named: "frame1.png");
+        //첫 이미지가 출력
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func btnR(_ sender: Any) {
+        count = 1
+        
+        lblC.text = ("\(count)")
+        imgView.image = UIImage(named: "frame\(count).png");
     }
-
-
+    
+    @IBAction func btnC(_ sender: Any) {
+        if count == 1 {
+            count = count + 1
+            
+            imgView.image = UIImage(named: "frame\(count).png")
+            lblC.text = ("\(count)")
+        } else if count == 2 {
+            count = count + 1
+            
+            imgView.image = UIImage(named: "frame\(count).png");
+            lblC.text = ("\(count)")
+        } else if count == 3 {
+            count = count + 1
+            
+            imgView.image = UIImage(named: "frame\(count).png");
+            lblC.text = ("\(count)")
+        } else if count == 4 {
+            count = count + 1
+            
+            imgView.image = UIImage(named: "frame\(count).png");
+            lblC.text = ("\(count)")
+        } else if count == 5 {
+            count = 1
+            
+            imgView.image = UIImage(named: "frame\(count).png");
+            lblC.text = ("\(count)")
+        }
+        
+        
+        
+    }
+    
+    @IBAction func btn2(_ sender: Any) {
+        if count == 1 {
+            d = true
+        } else if count == 5 {
+            d = false
+        }
+        
+        if d == true {
+            count = count + 1
+        } else if d == false {
+            count = count - 1
+        }
+        
+        imgView.image = UIImage(named: "frame\(count).png");
+        lblC.text = ("\(count)")
+    }
 }
 
